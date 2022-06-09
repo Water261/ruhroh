14556a74dede6e97cba1c98783866b68ce12e744
Matthew Wilks <water261@outlook.com.au>
2022-06-10 09:50:47 +1000

    feat(cli): Use clap_verbosity_flag
    Remove unused code and add error handling for simple_logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{path::PathBuf, process::exit, env, str::FromStr};
+use std::{path::PathBuf, process::exit};
 use clap::Parser;
-use log::{info, error, Level};
+use log::{info, error};
+use clap_verbosity_flag::{Verbosity, InfoLevel};
 use crate::config::file::Config;
 
 mod config;
@@ -12,33 +13,28 @@ pub struct CliArgs {
 	#[clap(short, long, parse(from_os_str))]
 	pub config_path: Option<PathBuf>,
 
-	/// Verbosity, default is error
-	#[clap(short, long)]
-	pub verbosity: Option<String>,
+	#[clap(flatten)]
+	pub verbosity: Verbosity<InfoLevel>,
 }
 
 const DEFAULT_CONFIG_PATH: &str = "/etc/ruhroh.conf";
-const DEFAULT_VERB_LVL: &str = "trace"; // TODO: Set to warn on release
 
 #[tokio::main]
 async fn main() {
 	let args = CliArgs::parse();
 	let config_path = args.config_path.unwrap_or_else(|| PathBuf::from(DEFAULT_CONFIG_PATH));
-	let verbosity_level = args.verbosity.unwrap_or_else(|| String::from(DEFAULT_VERB_LVL));
-	let log_level = Level::from_str(verbosity_level.as_str()).unwrap_or_else(|_| {
-		println!("Invalid verbosity level");
-		println!("Valid levels are: trace, debug, info, warn, error");
-		exit(1);
-	});
-	simple_logger::init_with_level(log_level).unwrap();
+	let verbosity_level = args.verbosity.log_level();
+
+	if let Some(log_level) = verbosity_level {
+		simple_logger::init_with_level(log_level).unwrap_or_else(|_| {
+			println!("Failed to initialise logger, exiting.");
+			exit(1);
+		});
+	}
 
 	info!("Loading configuration file {:?}", config_path.to_str());
 	let config = Config::from_path(config_path).unwrap_or_else(|_| {
 		error!("Failed to load config, changing to default.");
 		Config::default()
 	});
-
-	for device in config.devices {
-		
-	}
 }
